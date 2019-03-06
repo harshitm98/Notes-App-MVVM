ae9f672ff92c5075c02e0d8cff5d9d310a990561
harshitm98 <harshitmah98@gmail.com>
2019-03-06 13:55:03 +0530

    Implemented Room and Repository

---
+++ app/src/main/java/com/example/architecturecomponents/NoteDao.java
@@ -0,0 +1,29 @@
+package com.example.architecturecomponents;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
+@Dao
+public interface NoteDao {
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM note_table")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
+}
---
+++ app/src/main/java/com/example/architecturecomponents/NoteDatabase.java
@@ -0,0 +1,59 @@
+package com.example.architecturecomponents;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+
+// Want to create "singleton" Database
+
+@Database(entities = Note.class, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+    private static NoteDatabase instance;
+
+    // Room automatically generates code for this.
+    // So all you have to do is call this.
+    public abstract NoteDao noteDao();
+
+    // Synchronized make sures that only one thread can access it at one time.
+    // So in multi threading environment, multiple threads cannot access the database at same time.
+    public static synchronized NoteDatabase getInstance(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class,
+                    "note_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(room_callback)
+                    .build();
+        }
+        return instance;
+    }
+
+    private static RoomDatabase.Callback room_callback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDBAsyncTask(instance).execute();
+        }
+
+    };
+
+    private static class PopulateDBAsyncTask extends AsyncTask<Void, Void, Void>{
+        private NoteDao noteDao;
+
+        private PopulateDBAsyncTask(NoteDatabase db){
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1", 1));
+            noteDao.insert(new Note("Title 2", "Description 2", 2));
+            noteDao.insert(new Note("Title 3", "Description 3", 3));
+            return null;
+        }
+    }
+}
