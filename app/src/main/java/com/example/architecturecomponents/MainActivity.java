b0a8a2a5e17c25ecc4a116075b1eedd3c70cc94f
harshitm98 <harshitmah98@gmail.com>
2019-03-07 00:17:31 +0530

    Added RecyclerView

--- app/src/main/java/com/example/architecturecomponents/MainActivity.java
+++ app/src/main/java/com/example/architecturecomponents/MainActivity.java
@@ -5,6 +5,8 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -17,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         // We do not call new NoteViewModel because then we will have create new instance
         // with every activity. Instead we ask the Android system to create it for us.
         // Because the system knows when to create a new instance and provide and
@@ -26,8 +36,7 @@ public class MainActivity extends AppCompatActivity {
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
-                // update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
 
